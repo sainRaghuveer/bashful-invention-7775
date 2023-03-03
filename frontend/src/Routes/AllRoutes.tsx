fdfab87ec5d702ef0977e07aeda27bbaecd30747
unknown <NarayandasGanesh>
2023-03-04 03:10:09 +0530

    Added

--- frontend/src/Routes/AllRoutes.tsx
+++ frontend/src/Routes/AllRoutes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Heading from '../components/Ball/Heading'
 import HomePage from '../pages/HomePage'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
@@ -10,6 +11,7 @@ const AllRoutes = () => {
       <Route path='/' element={<HomePage />}></Route>
       <Route path='/register' element={<Register />}></Route>
       <Route path='/login' element={<Login />}></Route>
+      <Route path='/bouncing' element={<Heading/>}></Route>
     </Routes>
   )
 }
